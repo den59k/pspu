9d3faad2f975df42ff2ac64c59431d27a0822ddc
den59k <den59k@gmail.com>
2021-03-12 16:41:53 +0500

    a

--- editor/src/components/modal-window/index.js
+++ editor/src/components/modal-window/index.js
@@ -7,33 +7,22 @@ const Context = createContext()
 
 export function ModalWrapper ({children, className, style}){
 
-	const [ modalWindow, setModalWindow ] = useState(null)
 	const [ windowHistory, setWindowHistory ] = useState([])
 	const [ scroll, setScroll ] = useState(null)
 
 	const open = (_modal) => {
 		setScroll({left: window.pageXOffset, top: window.pageYOffset, offset: window.innerWidth-document.body.clientWidth})
 		setWindowHistory([ ...windowHistory, _modal ])
-		setModalWindow(_modal)
 		window.requestAnimationFrame(() => window.scroll(0, 0))
 	}
 
 	const close = () => {
-		setModalWindow(null)
-		
-		setWindowHistory(windowHistory => {
-			console.log(windowHistory)
-			if(windowHistory.length > 1)	
-				setModalWindow(windowHistory[windowHistory.length - 2])
-			return windowHistory.slice(0, -1)
-		})
-		
+		setWindowHistory(windowHistory => windowHistory.slice(0, -1))
 		window.requestAnimationFrame(() => window.scroll(scroll.left, scroll.top))
 	}
 
 	const closeAll = () => {
 		setWindowHistory([])
-		setModalWindow(null)
 		window.requestAnimationFrame(() => window.scroll(scroll.left, scroll.top))
 	}
 
@@ -46,16 +35,18 @@ export function ModalWrapper ({children, className, style}){
 		}		
 	}
 
+	const modalWindow = windowHistory.length > 0? windowHistory[windowHistory.length-1]: null
+	
 	return (
 		<Context.Provider value={{ open, close, closeAll }}>
 			<div 
-				className={cn( className, modalWindow !== null && styles.fixed )} 
+				className={cn( className, windowHistory.length > 0 && styles.fixed )} 
 				style={scroll?{...style, top: -scroll.top+'px', left: -scroll.left+'px', right: scroll.offset+'px'}: style}
 			>
 				{children}
 			</div>
 			
-			{ modalWindow !== null && (
+			{ windowHistory.length > 0 && (
 				<div className={styles.black} onMouseDown={onClick}>
 					{modalWindow}
 				</div>
